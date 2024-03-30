dd3525cd698a5e078a26e18020c37ce6ecbad35c
appdep <appdeployment64@gmail.com>
2024-03-30 09:56:45 -0700

    first commit

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -71,7 +71,7 @@ const ProductDetails = ({ product, products }) => {
       </div>
 
       <div className="maylike-products-wrapper">
-          <h2>You may also like</h2>
+          <h2>Choose Any Product</h2>
           <div className="marquee">
             <div className="maylike-products-container track">
               {products.map((item) => (
