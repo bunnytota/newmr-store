9035a63115a245d175612fde98628297e2dd228a
appdep <appdeployment64@gmail.com>
2024-03-31 06:34:05 -0700

    first commit

---
+++ components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import { Box, CircularProgress, Stack } from '@mui/material';
+
+const Loader = () =>  (
+ 
+         <div>
+            <h1>loading...</h1>
+         </div>
+
+);
+
+export default Loader;
